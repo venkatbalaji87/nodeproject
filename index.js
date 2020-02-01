926127ae2dda16a1b12bc725c5e166ec1607c870
venkatbalaji87 <venkatb2206@gmail.com>
2020-02-01 10:15:14 +0530

    node project

--- index.js
+++ index.js
@@ -2,18 +2,9 @@
 
 // req contains message , response  respond for the message
 const http = require("http");
-const url = require("url");
+const getQueryString = require("./getQueryString");
 
 //function
-const getURLRequest = urlString => {
-    try {
-        return url.parse(urlString, true).query
-    } catch(e) {
-        console.error(e);
-        return {};
-    }
-}
-
 http.createServer((request,response)=> {
     // if(request.url == "books") {
     //     //do some action regarding books.
@@ -21,8 +12,8 @@ http.createServer((request,response)=> {
     //response.writeHead(200, {'Content-Type': 'text/html'}); //Mime type
     response.writeHead(200, {"Content-Type": "application/json"});
    //response.write("Hello..");
-    const queryObject = getURLRequest(request.url);
+    const queryObject = getQueryString(request.url);
     //response.write(JSON.stringify({ status: "Success"}));
     response.write(JSON.stringify(queryObject));
     response.end();
-}).listen(9000);
+}).listen(8080);
